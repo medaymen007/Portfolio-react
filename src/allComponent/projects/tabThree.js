d86be8396ccca5c5783a2ad33dabd15d7f261005
medaymen007 <106028970+medaymen007@users.noreply.github.com>
2023-01-10 19:42:44 +0100

    Update tabThree.js

--- src/allComponent/projects/tabThree.js
+++ src/allComponent/projects/tabThree.js
@@ -7,12 +7,16 @@ import projectReactTwo from "../images/ReactProjectTwo.jpg"
 function TabThree(){
     return(
         <div className="tabThree">
+            <a href="">
             <div className="image">
             <img src={projectReactOne} alt="" />
             </div>
+            </a>
+            <a href="https://portfolio-react-fawn-nu.vercel.app/" target="_blank">
             <div className="image">
             <img src={projectReactTwo} alt="" />     
             </div>
+            </a>
         </div>
     )
 }
